c4507a8105594ec00503502f55450e4b1653d32b
Louis Thorpe-Monaghan <lthorpe-monaghan1@sheffield.ac.uk>
2016-04-30 20:55:30 +0100

    Added shield (primitive) and fire rate

--- ShefJam2Project/Assets/Scripts/MyPlayerController.cs
+++ ShefJam2Project/Assets/Scripts/MyPlayerController.cs
@@ -16,9 +16,12 @@ public class MyPlayerController : MonoBehaviour {
 	private float angle;
 	private float oldAngle = 0;
 	private Vector3 aimDirection;
+	public GameObject shield;
+	private float fireCooldown;
 
 	// Use this for initialization
 	void Start () {
+		fireCooldown = 1f;
 		distance = new Vector3(2,0,0);
 		thisTransform = transform;
 		startPos = thisTransform.position;
@@ -28,7 +31,7 @@ public class MyPlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		fireCooldown -= Time.deltaTime;
 
 		inputDirection = Vector3.zero;
 		inputDirection.x = Input.GetAxis("left joystick 1 horizontal");
@@ -52,25 +55,45 @@ public class MyPlayerController : MonoBehaviour {
 		aimer.transform.position = thisTransform.position + aimDirection;
 
 
-/*		inputDirectionAimer = Vector3.zero;
-		inputDirectionAimer.x = Input.GetAxis("right joystick 1 horizontal");
-		inputDirectionAimer.y = -(Input.GetAxis("right joystick 1 vertical"));
-		aimer.transform.position = thisTransform.position + inputDirectionAimer; */
 
+		if(Input.GetAxis("RT 1")>0.2){
+			if (thisTransform.childCount < 1){
+				if(fireCooldown < 0){
+					Fire();
+					fireCooldown = 1f;
+				}
+			}
+		} 
+		if (Input.GetAxis("RT 1") <= 0.2){
+			fireCooldown = 0;
+			}
+		
 
-		if(Input.GetAxis("RT 1")>0){
-			Fire();
+		if (thisTransform.childCount <1) {
+			if(Input.GetAxis("LT 1")>0){
+				Defend();
+				//InvokeRepeating("Defend", 0.00001f, 10f);
+			}
 		}
 
-		if(Input.GetAxis("LT 1")>0){
-			Fire();
+		if(Input.GetAxis("LT 1")<=0){
+			//shield = FindObjectOfType(GameObject);
+			foreach(Transform child in thisTransform){
+				Destroy(child.gameObject);
+			}
+			;
 		}
-
 	}
 
 	void Fire(){
-		GameObject laser = Instantiate(projectile,thisTransform.position, Quaternion.identity) as GameObject;
-		laser.GetComponent<Rigidbody2D>().velocity = aimDirection * laserSpeed;
+
+		GameObject projectileObj = Instantiate(projectile,thisTransform.position, Quaternion.identity) as GameObject;
+		projectileObj.GetComponent<Rigidbody2D>().velocity = aimDirection * laserSpeed;
 		//AudioSource.PlayClipAtPoint(fireSound, transform.position);
+	}
+
+	void Defend(){
+		GameObject shieldObj = Instantiate(shield, aimer.transform.position, Quaternion.identity) as GameObject;
+		shieldObj.transform.parent = GameObject.Find("Player 1").transform;
 	}
 }
