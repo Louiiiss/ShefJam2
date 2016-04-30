c21ac21ef5531b9b674426a107dba337f8458f25
Louis Thorpe-Monaghan <lthorpe-monaghan1@sheffield.ac.uk>
2016-05-01 00:21:15 +0100

    Added dashing, bullet collision, arena collision

--- ShefJam2Project/Assets/Scripts/MyPlayerController.cs
+++ ShefJam2Project/Assets/Scripts/MyPlayerController.cs
@@ -10,51 +10,90 @@ public class MyPlayerController : MonoBehaviour {
 	public GameObject aimer;
 	private Vector3 distance;
 	private Vector3 inputDirection;
+	private Vector3 rawInputDireciton; 
 	private Vector3 inputDirectionAimer;
 	public GameObject projectile;
-	public float laserSpeed = 25;
+	public float projectileSpeed = 6;
 	private float angle;
+	private float LSangle;
 	private float oldAngle = 0;
+	private float LSoldAngle = 0;
 	private Vector3 aimDirection;
+	private Vector3 shieldDirection;
 	public GameObject shield;
 	private float fireCooldown;
+	private Vector3 shieldDistance;
+	private float dashTime = 0;
+	private Vector3 dashDistance;
+	private Vector3 dashDirection;
+	private float dashSpeed = 10f;
+	private float dashCooldown;
 
 	// Use this for initialization
 	void Start () {
 		fireCooldown = 1f;
 		distance = new Vector3(2,0,0);
+		shieldDistance = new Vector3(0.6f,0,0);
+		dashDistance = new Vector3(3,0,0);
 		thisTransform = transform;
 		startPos = thisTransform.position;
 		//aimer = FindObjectOfType(GameObject);
 		aimer = Instantiate(aimer, thisTransform.position + distance, Quaternion.identity) as GameObject;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		fireCooldown -= Time.deltaTime;
+		dashTime -= Time.deltaTime;
+		dashCooldown -= Time.deltaTime;
+
+		if (dashTime <= 0){
+			thisTransform.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+		}
 
 		inputDirection = Vector3.zero;
 		inputDirection.x = Input.GetAxis("left joystick 1 horizontal");
 		inputDirection.y = -(Input.GetAxis("left joystick 1 vertical"));
-		thisTransform.position += inputDirection;
+
+
+		LSangle = Mathf.Atan2(Input.GetAxis("left joystick 1 horizontal"), Input.GetAxis("left joystick 1 vertical")) * Mathf.Rad2Deg;
+		// Dead Zone
+
+		if(Input.GetAxis("left joystick 1 horizontal") > 0.3 || Input.GetAxis("left joystick 1 vertical") > 0.3 || Input.GetAxis("left joystick 1 horizontal") < -0.3 || Input.GetAxis("left joystick 1 vertical") < -0.3){
+			LSoldAngle = LSangle;
+		}
+
+
+		if(Input.GetButtonDown("A 1 Button") && dashCooldown < 0){
+			dashTime = 0.2f;
+			Dash(LSoldAngle);
+		}
+		else if (dashTime < 0) {
+			thisTransform.position += inputDirection * 0.2f;
+			thisTransform.rotation = Quaternion.identity;
+		}
 
 
 		//Vector3 vNewInput = new Vector3(Input.GetAxis("right joystick 1 horizontal"), Input.GetAxis("right joystick 1 vertical"), 0.0f);
 		angle = Mathf.Atan2(Input.GetAxis("right joystick 1 horizontal"), Input.GetAxis("right joystick 1 vertical")) * Mathf.Rad2Deg;
-
-
 		// Dead Zone
 		if(Input.GetAxis("right joystick 1 horizontal") > 0.3 || Input.GetAxis("right joystick 1 vertical") > 0.3 || Input.GetAxis("right joystick 1 horizontal") < -0.3 || Input.GetAxis("right joystick 1 vertical") < -0.3){
 			oldAngle = angle;
 		}
 
 
+
 		aimDirection = Quaternion.Euler(0, 0, oldAngle - 90) * distance;
+		shieldDirection = Quaternion.Euler(0, 0, oldAngle - 90) * shieldDistance;
 
 
 		aimer.transform.position = thisTransform.position + aimDirection;
 
-
+		foreach(Transform child in thisTransform){
+			child.position = thisTransform.position + shieldDirection;
+			child.rotation = Quaternion.Euler(0, 0, oldAngle - 135);
+		}
+		
 
 		if(Input.GetAxis("RT 1")>0.2){
 			if (thisTransform.childCount < 1){
@@ -72,28 +111,32 @@ public class MyPlayerController : MonoBehaviour {
 		if (thisTransform.childCount <1) {
 			if(Input.GetAxis("LT 1")>0){
 				Defend();
-				//InvokeRepeating("Defend", 0.00001f, 10f);
 			}
 		}
 
 		if(Input.GetAxis("LT 1")<=0){
-			//shield = FindObjectOfType(GameObject);
 			foreach(Transform child in thisTransform){
 				Destroy(child.gameObject);
 			}
-			;
 		}
 	}
 
 	void Fire(){
 
-		GameObject projectileObj = Instantiate(projectile,thisTransform.position, Quaternion.identity) as GameObject;
-		projectileObj.GetComponent<Rigidbody2D>().velocity = aimDirection * laserSpeed;
+		GameObject projectileObj = Instantiate(projectile,(thisTransform.position + shieldDirection), Quaternion.identity) as GameObject;
+		projectileObj.GetComponent<Rigidbody2D>().velocity = aimDirection * projectileSpeed;
 		//AudioSource.PlayClipAtPoint(fireSound, transform.position);
 	}
 
 	void Defend(){
-		GameObject shieldObj = Instantiate(shield, aimer.transform.position, Quaternion.identity) as GameObject;
+		GameObject shieldObj = Instantiate(shield, (thisTransform.position + shieldDirection), Quaternion.Euler(0, 0, oldAngle - 135)) as GameObject;
 		shieldObj.transform.parent = GameObject.Find("Player 1").transform;
 	}
+
+	void Dash(float angle){
+		dashDirection = Quaternion.Euler(0, 0, angle - 90) * dashDistance;
+		thisTransform.GetComponent<Rigidbody2D>().velocity = dashDirection * dashSpeed;
+		dashCooldown = 3f;
+	}
+
 }
--- ShefJam2Project/Assets/Scripts/MyPlayerController2.cs
+++ ShefJam2Project/Assets/Scripts/MyPlayerController2.cs
@@ -12,11 +12,20 @@ public class MyPlayerController2 : MonoBehaviour {
 	private Vector3 inputDirection;
 	private Vector3 inputDirectionAimer;
 	public GameObject projectile;
-	public float laserSpeed = 25;
+	public float projectileSpeed = 6;
+	private float angle;
+	private float oldAngle = 0;
+	private Vector3 aimDirection;
+	private Vector3 shieldDirection;
+	public GameObject shield;
+	private float fireCooldown;
+	private Vector3 shieldDistance;
 
 	// Use this for initialization
 	void Start () {
-		distance = new Vector3(5,0,0);
+		fireCooldown = 1f;
+		distance = new Vector3(2,0,0);
+		shieldDistance = new Vector3(0.6f,0,0);
 		thisTransform = transform;
 		startPos = thisTransform.position;
 		//aimer = FindObjectOfType(GameObject);
@@ -25,35 +34,75 @@ public class MyPlayerController2 : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		fireCooldown -= Time.deltaTime;
 
 		inputDirection = Vector3.zero;
 		inputDirection.x = Input.GetAxis("left joystick 2 horizontal");
 		inputDirection.y = -(Input.GetAxis("left joystick 2 vertical"));
 		thisTransform.position += inputDirection;
+		thisTransform.rotation = Quaternion.identity;
 
-		inputDirectionAimer = Vector3.zero;
-		inputDirectionAimer.x = Input.GetAxis("right joystick 2 horizontal");
-		inputDirectionAimer.y = -(Input.GetAxis("right joystick 2 vertical"));
-		aimer.transform.position = thisTransform.position + inputDirectionAimer; 
+		//Vector3 vNewInput = new Vector3(Input.GetAxis("right joystick 1 horizontal"), Input.GetAxis("right joystick 1 vertical"), 0.0f);
+		angle = Mathf.Atan2(Input.GetAxis("right joystick 2 horizontal"), Input.GetAxis("right joystick 2 vertical")) * Mathf.Rad2Deg;
 
 
-		if(Input.GetAxis("RT 2")>0){
-			Fire();
-		}
-
-		if(Input.GetAxis("LT 2")>0){
-			Fire();
+		// Dead Zone
+		if(Input.GetAxis("right joystick 2 horizontal") > 0.3 || Input.GetAxis("right joystick 2 vertical") > 0.3 || Input.GetAxis("right joystick 2 horizontal") < -0.3 || Input.GetAxis("right joystick 2 vertical") < -0.3){
+			oldAngle = angle;
 		}
 
 
+		aimDirection = Quaternion.Euler(0, 0, oldAngle - 90) * distance;
+		shieldDirection = Quaternion.Euler(0, 0, oldAngle - 90) * shieldDistance;
+
+
+		aimer.transform.position = thisTransform.position + aimDirection;
+
+		foreach(Transform child in thisTransform){
+			child.position = thisTransform.position + shieldDirection;
+			child.rotation = Quaternion.Euler(0, 0, oldAngle - 135);
+		}
+		
+
+		if(Input.GetAxis("RT 2")>0.2){
+			if (thisTransform.childCount < 1){
+				if(fireCooldown < 0){
+					Fire();
+					fireCooldown = 1f;
+				}
+			}
+		} 
+		if (Input.GetAxis("RT 2") <= 0.2){
+			fireCooldown = 0;
+			}
+		
+
+		if (thisTransform.childCount <1) {
+			if(Input.GetAxis("LT 2")>0){
+				Defend();
+
+				//InvokeRepeating("Defend", 0.00001f, 10f);
+			}
+		}
+
+		if(Input.GetAxis("LT 2")<=0){
+			//shield = FindObjectOfType(GameObject);
+			foreach(Transform child in thisTransform){
+				Destroy(child.gameObject);
+			}
+			;
+		}
 	}
 
 	void Fire(){
-		GameObject laser = Instantiate(projectile,transform.position, Quaternion.identity) as GameObject;
-		laser.GetComponent<Rigidbody2D>().velocity = Vector3.up * laserSpeed;
+
+		GameObject projectileObj = Instantiate(projectile,(thisTransform.position + shieldDirection), Quaternion.identity) as GameObject;
+		projectileObj.GetComponent<Rigidbody2D>().velocity = aimDirection * projectileSpeed;
 		//AudioSource.PlayClipAtPoint(fireSound, transform.position);
+	}
 
-
+	void Defend(){
+		GameObject shieldObj = Instantiate(shield, (thisTransform.position + shieldDirection), Quaternion.Euler(0, 0, oldAngle - 135)) as GameObject;
+		shieldObj.transform.parent = GameObject.Find("Player 2").transform;
 	}
 }
